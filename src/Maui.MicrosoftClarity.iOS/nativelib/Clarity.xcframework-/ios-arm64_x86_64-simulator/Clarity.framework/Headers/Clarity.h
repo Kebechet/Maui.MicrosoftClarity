7b6be1d47e66165e0b25891afb9e9b90a4f480fb
Kebechet <samosidor@gmail.com>
2024-11-19 09:58:35 +0100

    replaced native lib with newer version

---
+++ src/Maui.MicrosoftClarity.iOS/nativelib/Clarity.xcframework-/ios-arm64_x86_64-simulator/Clarity.framework/Headers/Clarity.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for Clarity.
+FOUNDATION_EXPORT double ClarityVersionNumber;
+
+//! Project version string for Clarity.
+FOUNDATION_EXPORT const unsigned char ClarityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Clarity/PublicHeader.h>
+
+#import <Clarity/FloatParser.h>
